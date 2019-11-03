891bd9784ee4a79abebf2bd1956f6633168ddce7
Squaden <squaden.test@gmail.com>
2019-11-03 19:03:58 +0000

    Fixed credentials for VPS DO

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,11 +5,11 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',
-      region:              'us-east-2',
-      aws_access_key_id:     ENV['S3_ACCESS_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET_KEY']
+      region:              Rails.application.credentials[:S3_REGION],
+      aws_access_key_id:     Rails.application.credentials[:S3_ACCESS_KEY],
+      aws_secret_access_key: Rails.application.credentials[:S3_SECRET_KEY]
     }
 
-    config.fog_directory  = ENV['S3_BUCKET_NAME']
+    config.fog_directory  = Rails.application.credentials[:S3_BUCKET_NAME]
   end
 end
