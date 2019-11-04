643276cddbb9732154741d7d268a9766a2047582
Squaden <druzh.ds@gmail.com>
2019-11-04 17:23:29 +0300

    Add meta for yandex dns

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,13 +92,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = {host: 'bbqsq.herokuapp.com'}
+  config.action_mailer.default_url_options = {host: 'bbqsq.site'}
 
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :sendmail
 
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
